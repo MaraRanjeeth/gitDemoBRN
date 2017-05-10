a8eda31274f8e930f7e472d444a0a9b14de4ef7a
MaraRanjeeth <ranjithmara79@gmail.com>
2017-05-10 17:45:07 +0530

    dummy User Updated

--- getDemoTH/ViewController.swift
+++ getDemoTH/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let userName = "dumyUser"
+    let userName = "dumyUserChanged"
 
     override func viewDidLoad() {
         super.viewDidLoad()
